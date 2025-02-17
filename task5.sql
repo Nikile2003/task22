4fe0e38a83063d679670b24e91190eac05523d90
Nikile <150235099+Nikile2003@users.noreply.github.com>
2025-02-17 17:55:25 +0530

    Add files via upload

---
+++ task5.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE task_management;
+
+USE task_management;
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status ENUM('Pending', 'In Progress', 'Completed') DEFAULT 'Pending',
+    due_date DATE
+);
+select*from tasks where status='completed';
+
+select*from tasks;
